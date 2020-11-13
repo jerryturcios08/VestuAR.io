5cbc4c68f064062c5454fe8c6f0c6e230fab8290
Jerry Turcios <jerryturcios08@gmail.com>
2020-11-13 17:00:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# VestuAR.io
---
+++ VestuAR.io/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VestuAR.io
+//
+//  Created by Jerry Turcios on 11/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
